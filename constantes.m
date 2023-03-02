5e891930baf5d2a32d426fa291796ea22ee7b7c8
giuseppe lomonaco <giuseppe.lomonaco@usherbrooke.ca>
2023-03-02 16:24:28 -0500

    initial commit

---
+++ constantes.m
@@ -0,0 +1,13 @@
+%%  S5 - APP4 - PROBLEMATIQUE - CONSTANTES.M
+%   Auteur:     Giuseppe Lomonaco
+%   CIP:        LOMG2301
+%   Auteur:     Lucas Corrales
+%   CIP:        CORL0701
+
+%   Date:       2-MARS-2023
+%   Modifications (Date - initiales - détails):
+
+% Description: fichier contenant toutes les constantes necessaires 
+
+
+%%
---
+++ main.m
@@ -0,0 +1,17 @@
+%%  S5 - APP4 - PROBLEMATIQUE - MAIN.M
+%   Auteur:     Giuseppe Lomonaco
+%   CIP:        LOMG2301
+%   Auteur:     Lucas Corrales
+%   CIP:        CORL0701
+
+%   Date:       2-MARS-2023
+%   Modifications (Date - initiales - détails):
+
+
+%% MAIN CODE
+clc
+close all
+clear all
+
+% Initialisation
+constantes_lineaire % call le fichier des constantes
