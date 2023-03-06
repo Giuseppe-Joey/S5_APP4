f4e19def429b326bf159ad5612c430553151a3a0
giuseppe lomonaco <giuseppe.lomonaco@usherbrooke.ca>
2023-03-06 10:39:45 -0500

    validation

--- constantes.m
+++ constantes.m
@@ -27,3 +27,50 @@ gamma = teta - alpha;     %angle de vol de lavion (gamma = teta - alpha) et (gam
 
 
 
+
+
+
+
+%% ANNEXE A
+
+% instanciation des matrices 
+A = [   -0.018223   -0.088571  -9.78   0;...
+        -0.003038    -1.2563     0     1;...
+            0            0       0     1;...
+         0.0617       -28.075    0   -4.5937];
+
+
+B = [     0        1.1962;...
+          0       -0.00120;...
+          0           0;...
+          7.84      -4.05];
+
+
+C = [     1        0        0        0;...
+          0       57.296    0        0;...
+          0        0      57.296     0;...
+          0        0        0    57.296;...
+          0      -57.296    57.296   0];
+
+
+D = [   0   0;...
+        0   0;...
+        0   0;...
+        0   0;...
+        0   0];
+
+
+% Variables d'entree
+U = [delta_c   a_prop]' ;     %en degres et en fraction de la poussee maximale
+
+
+% Variables d'etat
+X = [v   alpha   teta   q]';     % en m/s et en radians
+
+
+% Variables de sortie
+Y = [v  alpha   teta    q   gamma]';     %en m/s et en degres
+
+
+
+
